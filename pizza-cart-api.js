10a30cb6e9e665935b5cd06c82e3f119515dda18
Sinenkosi Masinga <sinenkosimasinga@gmail.com>
2022-07-06 15:26:10 +0200

    initial commit

---
+++ pizza-cart-api.js
@@ -0,0 +1,20 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pizzaCartWithAPIWidget', function() {
+      return {
+
+        init(){
+            //alert('pizza loading..')
+            //call pizza API
+            axios
+                .get('https://pizza-cart-api.herokuapp.com/api/pizzas')
+                .then((result)=>{
+                    //console.log(result.data);
+                this.pizzas= result.data.pizzas
+        })
+        },
+        message: 'let eat pizza',
+        pizzas: []
+
+      }
+    });
+})
